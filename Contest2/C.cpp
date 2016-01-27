7e87b655ea356802dbb44d5c3a5c06257a797ea8
FidFenix <fidel1forever@gmail.com>
2016-01-27 03:44:59 -0500

    Very nice Problem ;) . I had to read STL one more time. again!

--- Contest2/C.cpp
+++ Contest2/C.cpp
@@ -1,22 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MP(i,j) make_pair(i,j)
-#define pb(i) push_back(i);
-#define f first
-#define s second
 int main(){
-    int n, m, k, N,num,q;
-    
-   while(scanf("%d %d",&n,&q) != EOF){
-    map<int,vector < int > > M;
-    for(int i = 1; i <= n; i++){
-        scanf("%d",&num);
-        M[num].pb(i);
-    }
-    while(q--){
-        scanf("%d %d",&k,&N);
-        if(k > M[N].size())printf("0\n");
-        else printf("%d\n",M[N][k-1]);
-    }
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+   int n, m, k,num,q;
+   while(1){
+       cin>>n>>m;
+       if(!(n+m)) break;
+       int arr[m];
+       for(int i = 0; i < m; i++)cin>>arr[i];
+       set< queue<int>,  greater< queue<int> > > S;
+       for(int i = 1; i < (1<<m); i++){
+           int sum = 0;
+           queue<int> res;
+           for(int j = 0; j < m; j++){
+               if(i & (1<<j)){
+                   res.push(arr[j]);
+                   sum += arr[j];
+               }
+           }
+           if(sum == n) S.insert(res);
+       }
+       cout<<"Sums of "<<n<<":"<<endl;
+       if(!S.size())cout<<"NONE"<<endl;
+       else for(queue<int> X: S){
+           while(!X.empty()){
+               cout<<X.front();
+               X.pop();
+               if(X.size())cout<<"+";
+           }
+           cout<<endl;
+       }
    }
 }
