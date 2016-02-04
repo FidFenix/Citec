1be1ce185adecfd70dff42e493f45417b7611b65
FidFenix <fidel1forever@gmail.com>
2016-02-04 00:39:06 -0500

    Tarea 4 DP

--- Homework4/B.cpp
+++ Homework4/B.cpp
@@ -1,25 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define N 1004
-string line1, line2;
+string line;
 int memo[N][N];
 int n;
 int f(int A, int B){
-    if(A >= n || B >= n)return 0;
+    if(A > B)return 0;
+    if(A == B)return 1;
     if(memo[A][B] != -1)return memo[A][B];
-    if(line1[A] == line2[B]) return memo[A][B] = 1 + f(A + 1, B + 1);
-    return memo[A][B] = max(f(A + 1, B), f(A, B + 1));
+    if(line[A] == line[B]) return memo[A][B] = 2 + f(A + 1, B - 1);
+    return memo[A][B] = max(f(A + 1, B), f(A, B - 1));
 }
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL);
     int t;
     cin>>t;
+    getline(cin,line);
     while(t--){
-        cin>>line1;
-        line2 = "";
-        n = line1.size();
-        for(int i = n -1; i >= 0; i--)line2 += line1[i];
+        getline(cin, line);
+        // A  MI NO ME AGARRAN 
+        if(!line.size()){
+            cout<<"0"<<endl;
+            continue;
+        }
+        n = line.size()-1;
         memset(memo,-1,sizeof memo);
-        cout<<f(0,0)<<endl;
+        cout<<f(0,n)<<endl;
     }
 }
